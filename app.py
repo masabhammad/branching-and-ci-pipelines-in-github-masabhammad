3c2d588d36f1e016169412623d60682f2f81a570
Muhammad Masab Hammad <136446395+mmasabalvi@users.noreply.github.com>
2025-08-30 23:51:10 +0500

    fixed linting again based on Actions

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.get("/")
 def home():
     return "Hello, World!"
 
+
 @app.get("/about")
 def about():
     return "About page"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,15 @@
 import app as myapp
 
+
 def test_home():
-    client = myapp.app.test_client()
-    r = client.get("/")
+    c = myapp.app.test_client()
+    r = c.get("/")
     assert r.status_code == 200
     assert b"Hello, World!" in r.data
 
+
 def test_about():
-    client = myapp.app.test_client()
-    r = client.get("/about")
+    c = myapp.app.test_client()
+    r = c.get("/about")
     assert r.status_code == 200
     assert b"About page" in r.data
